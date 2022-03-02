4df379d585073d95c37835bcb1f1514ab71e1a63
juan fajardo <carloschile20192019@gmail.com>
2022-03-01 22:20:00 -0300

    Gifsapp, Api en Angular para consumo de Gifs de diferentes motivos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import {SharedModule} from "./app/shared/shared.module";
+import {GifsModule} from "./gifs/gifs.module";
+import {HttpClientModule} from "@angular/common/http";
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    GifsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
